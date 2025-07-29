f0b94bf3c2890668a657003b1bb78567d1b964ef
DexZed <144045080+DexZed@users.noreply.github.com>
2025-07-29 21:13:05 +0600

    define parcel entity

---
+++ assignment-5/src/parcel module/parcelEntity/entity.ts
@@ -0,0 +1,92 @@
+import mongoose, { HydratedDocument, ObjectId } from "mongoose";
+
+interface IStatusLogSchema {
+  status:
+    | "Requested"
+    | "Approved"
+    | "Dispatched"
+    | "In Transit"
+    | "Delivered"
+    | "Cancelled"
+    | "Returned";
+  location?: string;
+  note?: string;
+}
+export interface IStatusLog extends HydratedDocument<IStatusLogSchema> {}
+
+const statusLogSchema = new mongoose.Schema<IStatusLog>(
+  {
+    status: {
+      type: String,
+      enum: [
+        "Requested",
+        "Approved",
+        "Dispatched",
+        "In Transit",
+        "Delivered",
+        "Cancelled",
+        "Returned",
+      ],
+      required: true,
+    },
+    location: { type: String },
+    note: { type: String },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    id: false,
+  }
+);
+
+interface IParcelSchema {
+  trackingId: string; // TRK-YYYYMMDD-xxxxxx
+  sender: ObjectId; // User
+  receiver: ObjectId; // User
+  type: "Document" | "Box" | "Fragile" | "Other";
+  weight: number;
+  pickupAddress: string;
+  deliveryAddress: string;
+  status: string;
+  statusLogs: IStatusLog[];
+  fee: number;
+  deliveryDate: Date;
+  isBlocked: boolean;
+}
+export interface IParcel extends HydratedDocument<IParcelSchema> {}
+
+const parcelSchema = new mongoose.Schema<IParcel>(
+  {
+    trackingId: { type: String, required: true, unique: true },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["Document", "Box", "Fragile", "Other"],
+      required: true,
+    },
+    weight: { type: Number, required: true },
+    pickupAddress: { type: String, required: true },
+    deliveryAddress: { type: String, required: true },
+    status: { type: String, required: true },
+    statusLogs: [statusLogSchema],
+    fee: { type: Number, required: true },
+    deliveryDate: { type: Date, required: true },
+    isBlocked: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const Parcel = mongoose.model<IParcel>("Package", parcelSchema);
+export default Parcel;
