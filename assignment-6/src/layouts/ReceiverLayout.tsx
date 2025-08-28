9edce1471638dd761d15fb26a4c17ab54e22b48c
DexZed <144045080+DexZed@users.noreply.github.com>
2025-08-28 21:17:58 +0600

    `feat: updated ReceiverLayout with new features and functionality`

--- assignment-6/src/layouts/ReceiverLayout.tsx
+++ assignment-6/src/layouts/ReceiverLayout.tsx
@@ -1,18 +1,38 @@
 import { Suspense } from "react";
-import { Link, Outlet } from "react-router";
+import { Link, Outlet, useLocation } from "react-router";
 import Skeleton from "../components/Skeleton";
+import { useAppSelector } from "../features/app/hooks";
+import AppContainer from "../components/Container";
+import Dash from "../components/Dash";
+import Footer from "../components/Footer";
+import SideBar from "../components/SideBar";
 // TODO: Fix styling and update route components
+
 export default function ReceiverLayout() {
-  return (
-    <div>
-      <nav>
-        <Link to="/receiver/incoming">📨 Incoming</Link> |{" "}
+  const selector = useAppSelector((state) => state.auth);
+  const location = useLocation();
+  const hideDash =
+    location.pathname.startsWith("/receiver/incoming") ||
+    location.pathname.startsWith("/receiver/history") 
+  const receiverLinks = (
+    <>
+      <li>
+        <Link to="/receiver/incoming">📨 Incoming</Link>
+      </li>
+      <li>
         <Link to="/receiver/history">🕓 History</Link>
-      </nav>
-      <hr />
-      <Suspense fallback={<Skeleton/>}>
-        <Outlet />
-      </Suspense>
-    </div>
+      </li>
+    </>
+  );
+  return (
+    <AppContainer>
+        <SideBar navLinks={receiverLinks}>
+          <Suspense fallback={<Skeleton />}>
+            {!hideDash && <Dash userData={selector}/>}
+            <Outlet />
+          </Suspense>
+        </SideBar>
+        <Footer />
+      </AppContainer>
   );
 }
