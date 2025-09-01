3dce6904fc9f0e139a12348d0f581ac20524cdeb
DexZed <144045080+DexZed@users.noreply.github.com>
2025-09-01 17:07:51 +0600

    `Added table with parcel information to SearchParcels page`

--- assignment-6/src/pages/SearchParcels.tsx
+++ assignment-6/src/pages/SearchParcels.tsx
@@ -5,6 +5,39 @@ type Props = {}
 function SearchParcels({}: Props) {
   return (
     <>
+    <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>Tracking ID</th>
+        <th>Sender</th>
+        <th>Receiver</th>
+        <th>Status</th>
+        <th>Fee</th>
+        <th>Type</th>
+        <th>Pickup Address</th>
+        <th>Delivery Address</th>
+        <th>Created At</th>
+      </tr>
+    </thead>
+    <tbody>
+      
+      {/* row 2 */}
+      <tr className="hover:bg-base-300">
+        <th>1232141</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+        <td>200</td>
+        <td>Other</td>
+        <td>123 avenue</td>
+        <td>456 avenue</td>
+        <td>26th May 2023</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
     </>
   )
 }
