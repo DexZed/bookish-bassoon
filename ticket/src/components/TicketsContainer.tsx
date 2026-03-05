2020864a37c85745c4786576f22cea9c44cf7f73
DexZed <144045080+DexZed@users.noreply.github.com>
2026-03-05 23:29:04 +0600

    `Removed unused import and useEffect hook, updated ticket filtering logic`

--- ticket/src/components/TicketsContainer.tsx
+++ ticket/src/components/TicketsContainer.tsx
@@ -1,28 +1,25 @@
-
-
 import { useTicketStore } from "../state/store";
 import Tickets from "./Tickets";
-import React, { useEffect } from "react";
+import React from "react";
 
 type Props = {};
 
 function TicketsContainer({}: Props) {
-  const {tickets,selectedTickets} = useTicketStore();
+  const { tickets } = useTicketStore();
 
-  const filteredTickets = tickets.filter((ticket) => ticket.status !== "Resolved");
-  useEffect(() => {
-    //console.log(filteredTickets);
-  }, [filteredTickets]);
- console.log(selectedTickets);
+  const filteredTickets = tickets.filter(
+    (ticket) => ticket.status !== "Resolved",
+  );
+
   return (
     <>
       <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        {filteredTickets.map((ticket)=>{
-          return(
-          <React.Fragment key={ticket.id}>
-              <Tickets  ticketProps={ticket}/>
-          
-          </React.Fragment>)
+        {filteredTickets.map((ticket) => {
+          return (
+            <React.Fragment key={ticket.id}>
+              <Tickets ticketProps={ticket} />
+            </React.Fragment>
+          );
         })}
       </section>
     </>
