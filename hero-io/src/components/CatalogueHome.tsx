79810bd3a85f5741caf723e6ee8e3e0c44e4741d
DexZed <144045080+DexZed@users.noreply.github.com>
2026-03-17 21:46:54 +0600

    `Added observable-hooks package and updated CatalogueHome component to use it`

--- hero-io/src/components/CatalogueHome.tsx
+++ hero-io/src/components/CatalogueHome.tsx
@@ -1,13 +1,22 @@
-
+import { from, map } from "rxjs";
 import { useAppData } from "../store/State";
 import Card from "./Card";
 import { Link } from "react-router";
+import { useObservableState } from "observable-hooks";
 
 type Props = {};
 
 function CatalogueHome({}: Props) {
-    const {state  } = useAppData();
-    console.log(state.data);
+  const { state } = useAppData();
+  const cardsData = state.data
+  const cards = cardsData.map((card) => ({
+    title: card.title,
+    downloads: card.downloads,
+    ratings: card.ratings.reduce((acc,curr)=> acc+curr.count,0),
+    image: card.image
+  }));
+  console.log(cards);
+  
   return (
     <>
       <section className="flex-centered-y p-4">
@@ -16,10 +25,14 @@ function CatalogueHome({}: Props) {
           <p>Explore all the Apps on the Market developed by us</p>
         </div>
         <div className="flex-centered-x flex-wrap lg:grid lg:grid-cols-4 gap-4 p-5">
-          {}
+          {cards.map((card, index) => (
+            <Card key={index} {...card} />
+          ))}
         </div>
         <div className="flex-centered-x">
-          <Link to="/apps" className="button-oulined btn-info">See All</Link>
+          <Link to="/apps" className="button-oulined btn-info">
+            See All
+          </Link>
         </div>
       </section>
     </>
@@ -27,4 +40,3 @@ function CatalogueHome({}: Props) {
 }
 
 export default CatalogueHome;
-
