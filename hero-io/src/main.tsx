fa36dde55834711063d9e903487e954a968218c3
DexZed <144045080+DexZed@users.noreply.github.com>
2026-03-23 23:11:39 +0600

    `Update import and routing configuration to use lazy loading and suspense`

--- hero-io/src/main.tsx
+++ hero-io/src/main.tsx
@@ -1,38 +1,39 @@
-import { StrictMode } from "react";
+import { lazy, StrictMode, Suspense, type JSX } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import ErrorPage from "./pages/ErrorPage.tsx";
-import Home from "./pages/Home.tsx";
-import Catalogue from "./pages/Catalogue.tsx";
-import Installations from "./pages/Installations.tsx";
-import Details from "./pages/Details.tsx";
 import Skeleton from "./components/Skeleton.tsx";
+
+const Home = lazy(() => import("./pages/Home.tsx"));
+const Catalogue = lazy(() => import("./pages/Catalogue.tsx"));
+const Installations = lazy(() => import("./pages/Installations.tsx"));
+const Details = lazy(() => import("./pages/Details.tsx"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage.tsx"));
+
+const suspenseWrap = (element: JSX.Element) => (
+  <Suspense fallback={<Skeleton />}>{element}</Suspense>
+);
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <ErrorPage />,
+    errorElement: suspenseWrap(<ErrorPage />),
     children: [
-      { index: true, element: <Home /> },
+      { index: true, element: suspenseWrap(<Home />) },
       {
         path: "/apps",
-        element: <Catalogue />,
+        element: suspenseWrap(<Catalogue />),
       },
       {
         path: "/installation",
-        element: <Installations />,
+        element: suspenseWrap(<Installations />),
       },
       {
         path: "/detail/:id",
-        element: <Details />,
-      },
-      {
-        path: "/loading",
-        element: <Skeleton/>,
+        element: suspenseWrap(<Details />),
       },
     ],
   },
